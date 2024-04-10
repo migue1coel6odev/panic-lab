b42865f3e48d95b62829f6f53993d593fdd95319
Miguel Coelho <miguelcoelhodev@gmail.com>
2024-04-10 22:17:36 +0100

    changed generate_board to use constants & build_amoeba

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
-use crate::{constants::{BLUE, ONE_EYED, ORANGE, PIECE_RED_LAB, PIECE_YELLOW_LAB, STRIPES, TWO_EYED}, game::PanicBoard};
+use crate::{
+    constants::{
+        BLUE, DOTS, ONE_EYED, ORANGE, PIECE_BLUE_LAB, PIECE_INVERT_COLOR, PIECE_INVERT_PATTERN,
+        PIECE_INVERT_SHAPE, PIECE_RED_LAB, PIECE_VENT, PIECE_YELLOW_LAB, STRIPES, TWO_EYED,
+    },
+    game::PanicBoard,
+};
 
 pub mod constants;
 pub mod game;
 
 fn main() {
-
     let board = generate_board();
 
     let mut panic_board = PanicBoard::new(board);
@@ -13,39 +18,36 @@ fn main() {
     let result = panic_board.check_amoeba_pos(false, PIECE_YELLOW_LAB, ONE_EYED, STRIPES, ORANGE);
 
     println!("Result: {result:#?}");
-
 }
-
 
 fn generate_board() -> [u8; 25] {
     let all_pieces: [u8; 25] = [
-        constants::PIECE_RED_LAB,
-        0b0000_0111,
-        constants::PIECE_INVERT_PATTERN,
-        0b0000_0100,
-        constants::PIECE_VENT,
-        0b0000_0110,
-        constants::PIECE_INVERT_SHAPE,
-        0b0000_0011,
-        0b0000_0001,
-        constants::PIECE_YELLOW_LAB,
-        0b0000_0110,
-        0b0000_0000,
-        constants::PIECE_VENT,
-        0b0000_0101,
-        constants::PIECE_INVERT_COLOR,
-        0b0000_0010,
-        0b0000_0011,
-        0b0000_0100, 
-        0b0000_0111,
-        constants::PIECE_BLUE_LAB,
-        0b0000_0010,
-        0b0000_0101,
-        0b0000_0000,
-        0b0000_0001,
-        constants::PIECE_VENT,
+        PIECE_RED_LAB,
+        PanicBoard::build_current_amoeba(TWO_EYED, BLUE, DOTS),
+        PIECE_INVERT_PATTERN,
+        PanicBoard::build_current_amoeba(TWO_EYED, STRIPES, ORANGE),
+        PIECE_VENT,
+        PanicBoard::build_current_amoeba(TWO_EYED, STRIPES, BLUE),
+        PIECE_INVERT_SHAPE,
+        PanicBoard::build_current_amoeba(ONE_EYED, DOTS, BLUE),
+        PanicBoard::build_current_amoeba(ONE_EYED, ORANGE, DOTS),
+        PIECE_YELLOW_LAB,
+        PanicBoard::build_current_amoeba(TWO_EYED, BLUE, STRIPES),
+        PanicBoard::build_current_amoeba(ONE_EYED, ORANGE, STRIPES),
+        PIECE_VENT,
+        PanicBoard::build_current_amoeba(TWO_EYED, ORANGE, DOTS),
+        PIECE_INVERT_COLOR,
+        PanicBoard::build_current_amoeba(ONE_EYED, BLUE, STRIPES),
+        PanicBoard::build_current_amoeba(ONE_EYED, DOTS, BLUE),
+        PanicBoard::build_current_amoeba(TWO_EYED, STRIPES, ORANGE),
+        PanicBoard::build_current_amoeba(TWO_EYED, BLUE, DOTS),
+        PIECE_BLUE_LAB,
+        PanicBoard::build_current_amoeba(ONE_EYED, STRIPES, BLUE),
+        PanicBoard::build_current_amoeba(TWO_EYED, ORANGE, DOTS),
+        PanicBoard::build_current_amoeba(ONE_EYED, STRIPES, ORANGE),
+        PanicBoard::build_current_amoeba(ONE_EYED, DOTS, ORANGE),
+        PIECE_VENT,
     ];
 
     all_pieces
-
 }
