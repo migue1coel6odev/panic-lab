e07394d2ceaf1177e1598fe2e8c412ed3553a55b
Miguel Coelho <miguelcoelhodev@gmail.com>
2024-04-10 21:34:49 +0100

    board reverse | max lab go through 4

--- src/game.rs
+++ src/game.rs
@@ -1,68 +1,95 @@
-use crate::constants::{PIECE_VENT, PIECE_INVERT_COLOR, INVERT_COLOR, PIECE_INVERT_PATTERN, INVERT_PATTERN, PIECE_INVERT_SHAPE, INVERT_SHAPE, PIECE_BLUE_LAB, PIECE_RED_LAB, PIECE_YELLOW_LAB};
+use crate::constants::{
+    INVERT_COLOR, INVERT_PATTERN, INVERT_SHAPE, PIECE_BLUE_LAB, PIECE_INVERT_COLOR,
+    PIECE_INVERT_PATTERN, PIECE_INVERT_SHAPE, PIECE_RED_LAB, PIECE_VENT, PIECE_YELLOW_LAB,
+};
 
 pub struct PanicBoard {
     pub board: [u8; 25],
     current_pos: usize,
     current_amoeba: u8,
+    clockwise: bool,
 }
 
 impl PanicBoard {
-
-    pub fn new (board: [u8; 25]) -> PanicBoard {
+    pub fn new(board: [u8; 25]) -> PanicBoard {
         PanicBoard {
             board,
             current_pos: 0,
-            current_amoeba: 0b0000_0000
+            current_amoeba: 0b0000_0000,
+            clockwise: true
         }
     }
 
+    pub fn set_clockwise(&mut self, is_clockwise: bool) {
+        self.clockwise = is_clockwise;
+    }
+
+    pub fn set_current_amoeba(&mut self, new_current_amoeba: u8) {
+        self.current_amoeba = new_current_amoeba;
+    }
+
     pub fn set_current_pos(&mut self, new_current_pos: usize) {
-        self.current_pos =  new_current_pos;
+        self.current_pos = new_current_pos;
     }
 
     pub fn check_amoeba_pos(&mut self) -> Result<usize, ()> {
-        
+        let mut count_mutations: u8 = 0;
 
         let mut current_pos: usize = self.current_pos;
 
+        let mut board = self.board;
+        if self.clockwise == false {
+            board.reverse();
+        }
+
         loop {
-            match self.board[current_pos] {
-                PIECE_VENT => self.get_next_vent(),
-                PIECE_INVERT_COLOR => self.current_amoeba ^= INVERT_COLOR,
-                PIECE_INVERT_PATTERN => self.current_amoeba ^= INVERT_PATTERN,
-                PIECE_INVERT_SHAPE => self.current_amoeba ^= INVERT_SHAPE,
-                PIECE_BLUE_LAB => {}, 
-                PIECE_RED_LAB => {},
-                PIECE_YELLOW_LAB => {},
+            match board[current_pos] {
+                PIECE_VENT => current_pos = self.get_next_vent(current_pos),
+                PIECE_INVERT_COLOR => {
+                    count_mutations += 1;
+                    self.current_amoeba ^= INVERT_COLOR
+                }
+                PIECE_INVERT_PATTERN => {
+                    count_mutations += 1;
+                    self.current_amoeba ^= INVERT_PATTERN
+                }
+                PIECE_INVERT_SHAPE => {
+                    count_mutations += 1;
+                    self.current_amoeba ^= INVERT_SHAPE
+                }
+                PIECE_BLUE_LAB => {}
+                PIECE_RED_LAB => {}
+                PIECE_YELLOW_LAB => {}
                 amoeba => {
                     if amoeba.eq(&self.current_amoeba) {
                         break;
                     }
                 }
             }
+
+            if count_mutations == 4 {
+                break;
+            }
+
             current_pos += 1;
             if current_pos == self.board.len() {
                 current_pos = 0;
             }
-        
         }
- 
-        Ok(current_pos)
 
+        Ok(current_pos)
     }
 
-    fn get_next_vent (&mut self) {
-        let mut current_pos = self.current_pos;
+    fn get_next_vent(&self, current_pos: usize) -> usize {
+        let mut next_current_pos = current_pos + 1;
+
         loop {
-            match self.board[current_pos] {
-                PIECE_VENT => {
-                    self.current_pos = current_pos;
-                    break
-                },
-                _ => current_pos += 1
+            match self.board[next_current_pos] {
+                PIECE_VENT => break,
+                _ => next_current_pos += 1,
             }
         }
+
+        next_current_pos
     }
-
-
 }
--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,9 @@ fn main() {
 
     let mut panic_board = PanicBoard::new(board);
 
-    panic_board.set_current_pos(1);
+    panic_board.set_current_pos(9);
+    panic_board.set_current_amoeba(0b0000_0101);
+    panic_board.set_clockwise(false);
 
     let result = panic_board.check_amoeba_pos();
 
@@ -20,31 +22,31 @@ fn main() {
 
 fn generate_board() -> [u8; 25] {
     let all_pieces: [u8; 25] = [
-        constants::PIECE_VENT,
-        constants::PIECE_VENT,
-        constants::PIECE_VENT,
-        constants::PIECE_BLUE_LAB,
         constants::PIECE_RED_LAB,
-        constants::PIECE_YELLOW_LAB,
-        constants::PIECE_INVERT_COLOR,
+        0b0000_0111,
         constants::PIECE_INVERT_PATTERN,
-        constants::PIECE_INVERT_SHAPE,
-        0b0000_0000,
-        0b0000_0000,
-        0b0000_0001,
-        0b0000_0001,
-        0b0000_0010,
-        0b0000_0010,
-        0b0000_0011,
-        0b0000_0011,
         0b0000_0100,
+        constants::PIECE_VENT,
+        0b0000_0110,
+        constants::PIECE_INVERT_SHAPE,
+        0b0000_0011,
+        0b0000_0001,
+        constants::PIECE_YELLOW_LAB,
+        0b0000_0110,
+        0b0000_0000,
+        constants::PIECE_VENT,
+        0b0000_0101,
+        constants::PIECE_INVERT_COLOR,
+        0b0000_0010,
+        0b0000_0011,
         0b0000_0100, 
-        0b0000_0101,
-        0b0000_0101,
-        0b0000_0110,
-        0b0000_0110,
         0b0000_0111,
-        0b0000_0111,
+        constants::PIECE_BLUE_LAB,
+        0b0000_0010,
+        0b0000_0101,
+        0b0000_0000,
+        0b0000_0001,
+        constants::PIECE_VENT,
     ];
 
     all_pieces
